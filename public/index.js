326238412450d76feeac8aedcca6df66b42228ba
NygilNet <nygilnet@gmail.com>
2022-12-20 15:32:21 -0600

    header and working on image tag

--- public/index.js
+++ public/index.js
@@ -2,16 +2,37 @@
 
 window.onload = () => {
   start();
-
+  const data = getPic()
+    .then(r => r);
+  console.log(data);
+  console.log(data[0])
+  const link = data[0].url;
+  console.log(link);
+  const img = document.createElement('img');
+  img.src = link
+  document.body.appendChild(img);
 }
 
 
 function start(){
     const header = document.createElement('h1');
     header.innerText = "Kitten Pic";
+    header.style.display = "flex";
+    header.style.justifyContent = 'center';
     document.body.appendChild(header)
 }
 
-function getPic(){
-    
+async function getPic(){
+  try {
+    let resp = await fetch('https://api.thecatapi.com/v1/images/search')
+      let data = await resp.json();
+      // console.log(data[0].url.split('.'))
+      let ext = data[0].url.split('.')[3]
+      if (ext === 'png' || ext === 'jpg') return data;
+
+      return getPic();
+  } catch(e) {
+    console.error(e);
+  }
+
 }
