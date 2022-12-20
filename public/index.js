5ced3e03bce80290c885d2ab8cc7bb3f3102837b
Dylan Godeck <Dylanj.Godeck@gmail.com>
2022-12-20 12:51:55 -0800

    First commit

--- public/index.js
+++ public/index.js
@@ -1,1 +1,17 @@
 // Your code here
+
+window.onload = () => {
+  start();
+
+}
+
+
+function start(){
+    const header = document.createElement('h1');
+    header.innerText = "Kitten Pic";
+    document.body.appendChild(header)
+}
+
+function getPic(){
+    
+}
