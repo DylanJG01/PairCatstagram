92cbb6a9b98f43b818d25dc79fae5f77dd20f612
Dylan Godeck <Dylanj.Godeck@gmail.com>
2022-12-21 14:27:57 -0800

    cat pics refresh on click

--- public/index.js
+++ public/index.js
@@ -1,63 +1,87 @@
 // Your code here
+window.addEventListener("DOMContentLoaded", event => {
 
-window.onload = () => {
-    start();
-    makeDiv("container");
+
+  start();
+
+  makeDiv("container");
+  const mainContainer = document.getElementById("container")
+  mainContainer.style.display = "flex";
+  mainContainer.style.justifyContent = "center";
+
+  picContainer();
+
+  let img = document.getElementById('container');
+
+  const changeImg = e => {
+
+
+    // document.getElementById("container").remove();
+    // makeDiv("container");
     // const mainContainer = document.getElementById("container")
-    // appendTo(mainContainer, getPic())
-    const mainContainer = document.getElementById("container")
+    // mainContainer.style.display = "flex";
+    // mainContainer.style.justifyContent = "center";
+
+    // document.querySelector("img").remove()
+
     getPic();
-    
-    mainContainer.style.display = "flex";
-    mainContainer.style.justifyContent = "center";
-//   const data = getPic()
-//     .then(r =>  r)
 
-//   console.log(data);
-//   console.log(data[0], "Data")
+  }
 
-  
+  img.addEventListener('click', changeImg);
+
+
+});
+
+
+function start() {
+  const header = document.createElement('h1');
+  header.innerText = "Kitten Pic";
+  header.style.display = "flex";
+  header.style.justifyContent = 'center';
+  document.body.appendChild(header)
 }
 
-
-function start(){
-    const header = document.createElement('h1');
-    header.innerText = "Kitten Pic";
-    header.style.display = "flex";
-    header.style.justifyContent = 'center';
-    document.body.appendChild(header)
-}
-
-async function getPic(){
+async function getPic() {
   try {
     let resp = await fetch('https://api.thecatapi.com/v1/images/search')
-      let data = await resp.json();
-      // console.log(data[0].url.split('.'))
+    let data = await resp.json();
+    // console.log(data[0].url.split('.'))
+    //console.log(data[0].url)
+    // console.log(mainContainer)
+    const img = document.getElementById("cat-pic")
+    img.src = data[0].url
+    // return data[0].url;
 
-    //   console.log(data[0].url)
-
-        const img = document.createElement('img');
-        img.src = data[0].url
-        const mainContainer = document.getElementById("container")
-        mainContainer.appendChild(img);
-        return data[0].url;
-
-  } catch(e) {
+  } catch (e) {
     console.error(e);
   }
 
 }
 
-function makeDiv(id) {
-   const div = document.createElement('div')
+function picContainer() {
 
-   if(id !== undefined){
-    div.id = id
-   }
-   appendTo(document.body, div)
+  const img = document.createElement('img');
+  img.style.width = '800px';
+  img.style.height = '450px';
+  img.style.setProperty(":hover",)
+  img.id = 'cat-pic';
+  getPic()
+
+  const mainContainer = document.getElementById("container")
+  mainContainer.appendChild(img);
 }
 
-function appendTo(div, ...appended){
+function makeDiv(id) {
+  const div = document.createElement('div')
 
-    div.append(...appended)
+  if (id !== undefined) {
+    div.id = id
+  }
+  appendTo(document.body, div)
+}
+
+function appendTo(div, ...appended) {
+
+  div.append(...appended)
 }
